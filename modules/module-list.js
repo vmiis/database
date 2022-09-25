228acd7fdc1979e64f2a83c166c53fe9a79aec4a
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-09-26 05:02:56 +1000

    Update module-list.js

--- modules/module-list.js
+++ modules/module-list.js
@@ -6,5 +6,5 @@
     m["Data"]                             ={url:H+"/tasks/Database/Data/page.html",form_module:"Details",router:1};
     m["Details"]                          ={url:H+"/tasks/Database/Details/page.html"};
     m["About"]                            ={url:"http://localhost:8000/vmiis/web-portal/modules/shared/microapp/page.html",router:1};
-    //m["About"]                          ={url:"https://www.vmiis.com/modules/shared/microapp/page.html",router:1};
+    m["About"]                            ={url:"https://www.vmiis.com/modules/shared/microapp/page.html",router:1};
 })();
